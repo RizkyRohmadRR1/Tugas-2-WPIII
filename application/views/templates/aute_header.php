81909707703a45fc20eac409fe95b50de7115529
RizkyRohmadRR1 <Bagogo1928@gmail.com>
2022-03-22 23:38:53 +0700

    Commit

--- application/views/templates/aute_header.php
+++ application/views/templates/aute_header.php
@@ -4,30 +4,20 @@
 <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>Pustaka-Booking | <?= $judul; ?></title>
-    <link rel="icon" type="image/png" href="<?= base_url('assets/img/logo/'); ?> logo-pb.png ">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/user/css/bootstrap.css">
+
+    <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="<?= base_url('assets/'); ?>datatable/datatable.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light  bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="<?= base_url(); ?>">Pustaka</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">    
-                <a class="nav-item-nav-link active" href="<? base_url(); ?>">Beranda<span class="sr-only">(current)</span></a>
-                <a class="nav-item-nav-link" href="<? base_url(); ?>Mahasiswa">Mahasiswa</a>
-                <a class="nav-item-nav-link" href="<? base_url(); ?>About">About</a>
-                <a class="nav-item-nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-    </div class="container mt-5">            
+<body class="bg-gradient-primary">
